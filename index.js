4c5ec16d926eefa4ffe3a7490aaf63b61838b8a6
Art Deineka <art.deineka@darkruby.co.uk>
2016-09-13 13:18:09 +0100

    added some tests

--- index.js
+++ index.js
@@ -1,13 +1,11 @@
 const dataPrefix = '__DATA';
 const indexSuffix = '__IDX_';
 
-const generateVar = function(name) {
-  return `_${name}${indexSuffix}`.split('.').join('__');
-}
+const generateVar = name => `_${name}${indexSuffix}`.split('.').join('__');
 
-const convertTokens = function(expr) {
+const convertTokens = (expr) => {
   const tokens = expr.match(/".+?"|'.+?'|[()]+|#?[\w\[\]\.]+|[!=<>]+/g);
-  const convertedTokens = tokens.map(token => {
+  const convertedTokens = tokens.map((token) => {
     switch (token) {
       case 'and':
         return '&&';
@@ -36,16 +34,17 @@ const convertTokens = function(expr) {
     }
   });
   return convertedTokens.join(' ');
-}
+};
 
-const convertEachLoops = function(text) {
+const convertEachLoops = (text) => {
+  let result = text;
   const nestedNames = {};
   const blockRegex = /{{\s*?each\s+?.+?\s*?}} *\n?|{{.*?loop_vars\..+?.*?}}|{{.*?loop_index.*?}}/g;
   let match;
   let eachIndex;
 
   // eslint-disable-next-line
-  while ((match = blockRegex.exec(text)) !== null) {
+  while ((match = blockRegex.exec(result)) !== null) {
     const matched = match[0];
     const eachMatch = matched.match(/{{\s*?each\s+?(.+?)\s*?}} *\n?/);
     const loopMatch = matched.match(/{{(.*?)(loop_vars\.\w+)(.*?)}}|({{.*)loop_index(.*?}})/);
@@ -56,8 +55,7 @@ const convertEachLoops = function(text) {
       if (!expr.startsWith('loop_vars.')) {
         eachIndex = generateVar(expr);
         nestedNames[`loop_vars.${expr}`] = expr;
-        // eslint-disable-next-line
-        text = `${text.substring(0, match.index)}<% for (var ${eachIndex} in ${dataPrefix}.${expr}) { %>${text.substring(match.index + eachMatch[0].length)}`;
+        result = `${result.substring(0, match.index)}<% for (var ${eachIndex} in ${dataPrefix}.${expr}) { %>${result.substring(match.index + eachMatch[0].length)}`;
       } else {
         const nestedProps = expr.split('.').slice(1);
         const parentIndex = generateVar(nestedProps[0]);
@@ -68,47 +66,35 @@ const convertEachLoops = function(text) {
         nestedNames[`loop_vars.${nestedProps[nestedProps.length - 1]}`] =
           `${nestedProps[0]}[${generateVar(nestedProps[0])}].${nestedProps.slice(1).join('.')}`;
 
-        // eslint-disable-next-line
-        text = `${text.substring(0, match.index)}<% for (var ${childIndex} in ${exprTranslation}[${parentIndex}].${nestedProps.slice(1).join('.')}) { %>${text.substring(match.index + eachMatch[0].length)}`;
+        result = `${result.substring(0, match.index)}<% for (var ${childIndex} in ${exprTranslation}[${parentIndex}].${nestedProps.slice(1).join('.')}) { %>${result.substring(match.index + eachMatch[0].length)}`;
       }
     } else if (loopMatch) {
+      // eslint-disable-next-line
       const replacement = loopMatch[0].replace(loopRegex, (expr, isLoopVar, nestedProps, isLoopIndex = false) => {
-        if (!!isLoopVar) {
+        if (isLoopVar) {
           const exprKey = expr.split('.').slice(0, 2).join('.');
           const childIndex = generateVar(nestedProps);
           const exprTranslation = nestedNames[exprKey];
           const isPrint = !/\s*/.exec(loopMatch[1]);
           return `${isPrint ? '<%-' : ''}${exprTranslation}[${childIndex}]${isPrint ? '%>' : ''}`;
-        } else if (!!isLoopIndex) {
+        } else if (isLoopIndex) {
           return eachIndex;
         }
       });
-      text = `${text.substring(0, match.index)}${replacement}${text.substring(match.index + loopMatch[0].length)}`;
+      result = `${result.substring(0, match.index)}${replacement}${result.substring(match.index + loopMatch[0].length)}`;
     }
   }
-  return text;
-}
-
-const convertExpressions = function(text) {
-  return text
-    .replace(/{{\s*?if(\s+?.+?|\(.+?)}} *\n?/g, (matches, expr) => {
-      return `<% if (${convertTokens(expr)}) { %>`;
-    })
-    .replace(/{{\s*?else\s*?}} *\n?/g, '<% } else { %>')
-    .replace(/{{\s*?elseif(\s+?.+?|\(.+?)}} *\n?/g, (matches, expr) => {
-      return `<% } else if (${convertTokens(expr)}) { %>`;
-    })
-    .replace(/{{\s*?end\s*?}} *\n?/g, '<% } %>')
-    .replace(/{{{\s*?(?!\s*if|else|each|elseif[\s\(}]+?)(.*?)}}}/g, (matches, expr) => {
-      return `<%= ${convertTokens(expr)} %>`;
-    })
-    .replace(/{{\s*?(?!\s*if|else|each|elseif[\s\(}]+?)(.*?)}}/g, (matches, expr) => {
-      return `<%- ${convertTokens(expr)} %>`;
-    });
-}
-
-exports.lodashConverter = function (text) {
-  return convertExpressions(convertEachLoops(text));
+  return result;
 };
 
+const convertExpressions = text => text
+    .replace(/{{\s*?if(\s+?.+?|\(.+?)}} *\n?/g, (matches, expr) => `<% if (${convertTokens(expr)}) { %>`)
+    .replace(/{{\s*?else\s*?}} *\n?/g, '<% } else { %>')
+    .replace(/{{\s*?elseif(\s+?.+?|\(.+?)}} *\n?/g, (matches, expr) => `<% } else if (${convertTokens(expr)}) { %>`)
+    .replace(/{{\s*?end\s*?}} *\n?/g, '<% } %>')
+    .replace(/{{{\s*?(?!\s*if|else|each|elseif[\s\(}]+?)(.*?)}}}/g, (matches, expr) => `<%= ${convertTokens(expr)} %>`)
+    .replace(/{{\s*?(?!\s*if|else|each|elseif[\s\(}]+?)(.*?)}}/g, (matches, expr) => `<%- ${convertTokens(expr)} %>`);
+
+
+exports.lodashConverter = text => convertExpressions(convertEachLoops(text));
 exports.dataPrefix = dataPrefix;
